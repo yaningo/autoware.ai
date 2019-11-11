108780d448d4f18ab23910b337c7229470de87ef
qswawrq <qswawrq@gmail.com>
2019-11-11 06:35:54 -0800

    update file path

--- docker/install.sh
+++ docker/install.sh
@@ -3,4 +3,4 @@ source /home/carma/.base-image/init-env.sh
 export ROS_LANG_DISABLE=genjava # Disable genjava as it is not needed in this image and makes build inconsistent 
 autoware_src="/home/carma/autoware.ai"
 cd ${autoware_src}/ros 
-./carma_autoware_build -a ${autoware_src}
+../autoware/ros/carma_autoware_build -a ${autoware_src}
