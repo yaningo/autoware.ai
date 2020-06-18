769f3a56247b4af5efdb574081e7262f10d09056
jtbaird <john.baird@leidos.com>
2020-06-18 13:49:24 -0400

    added set -x

--- docker/install.sh
+++ docker/install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -x
+
 source /home/carma/.base-image/init-env.sh
 export ROS_LANG_DISABLE=genjava # Disable genjava as it is not needed in this image and makes build inconsistent 
 autoware_src="/home/carma/autoware.ai"
