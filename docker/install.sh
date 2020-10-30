6a9007a9495df6ad550e5dd22228845237d07c7f
Misheel Bayartsengel <misheel.bayart@gmail.com>
2020-10-30 09:03:15 -0400

    remove remaining java (#123)
    * initial java removal
    
    * try fix build

--- docker/install.sh
+++ docker/install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 source /home/carma/.base-image/init-env.sh
-export ROS_LANG_DISABLE=genjava # Disable genjava as it is not needed in this image and makes build inconsistent 
 autoware_src="/home/carma/autoware.ai"
 cd ${autoware_src}
 ./autoware/ros/carma_autoware_build -a ${autoware_src}
