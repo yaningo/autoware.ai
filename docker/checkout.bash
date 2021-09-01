ec2570e7fb6e776b98c94f3e3ba1c24c9fbcdf69
SaikrishnaBairamoni <saikrishna.bairamoni@leidos.com>
2021-09-01 14:31:55 -0400

    update config and Dockerfile for a1

--- docker/checkout.bash
+++ docker/checkout.bash
@@ -35,7 +35,7 @@ done
 cd ${dir}/autoware.ai
 
 if [[ "$BRANCH" = "carma-develop" ]]; then
-      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-msgs.git --branch develop
+      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-msgs.git --branch release/a1
 else
-      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-msgs.git --branch develop
+      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-msgs.git --branch release/a1
 fi
