cf6df553fd4665f90dcc2c3d66462faf47065f29
SaikrishnaBairamoni <saikrishna.bairamoni@leidos.com>
2022-01-18 09:01:07 -0500

    update dockerfiles to point candidate

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM usdotfhwastoldev/carma-base:develop as base_image
+FROM usdotfhwastolcandidate/carma-base:fiesta as base_image
 
 FROM base_image as build
 
--- docker/checkout.bash
+++ docker/checkout.bash
@@ -36,7 +36,7 @@ cd ${dir}/autoware.ai
 if [[ "$BRANCH" = "carma-develop" ]]; then
       git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-msgs.git --branch $BRANCH
 else
-      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-msgs.git --branch develop
+      git clone --depth=1 https://github.com/usdot-fhwa-stol/carma-msgs.git --branch release/fiesta
 fi
 
 # Required to build pacmod_msgs
